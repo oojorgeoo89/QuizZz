ceef05528fb332342b9dda4f6dfb91d261f42a06
Jorge Rodriguez <jorgerodriguez827@gmail.com>
2017-06-23 22:51:33 +0200

    Enable form login and remember-me functionality via token repository.

--- src/main/java/jorge/rv/quizzz/config/SecurityConfig.java
+++ src/main/java/jorge/rv/quizzz/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package jorge.rv.quizzz.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,16 +12,41 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	  http.httpBasic();
-	  http.csrf().disable();
+	  http
+	  	.httpBasic()
+	  .and()
+	  	.formLogin()
+	  		.defaultSuccessUrl("/", true)
+	  .and()
+	  	.rememberMe()
+			.tokenRepository(persistentTokenRepository())
+	  .and()
+	  	.csrf()
+	  		.disable()
+	  	.logout()
+	  		.logoutSuccessUrl("/")
+	  		.deleteCookies("JSESSIONID")
+	  		.invalidateHttpSession(true) ;
+	}
+	
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
+		db.setDataSource(dataSource);
+		return db;
 	}
 	
 	@Autowired
